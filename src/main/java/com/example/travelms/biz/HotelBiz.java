830063ad057c8836cdf8178a1165bd6dca9dbe89
2451850062 <2451850062@qq.com>
2018-06-15 10:06:22 +0800

    酒店管理

--- src/main/java/com/example/travelms/biz/HotelBiz.java
+++ src/main/java/com/example/travelms/biz/HotelBiz.java
@@ -1,8 +1,15 @@
 package com.example.travelms.biz;
 
+import com.example.travelms.entity.Continent;
+import com.example.travelms.entity.Country;
 import com.example.travelms.entity.Hotel;
 import com.example.travelms.util.Pages;
 
+import java.util.List;
+
 public interface HotelBiz {
     public Pages<Hotel> listHotel(Hotel hotel, Integer pageIndex, Integer pageSize);
+    boolean delByhotelId(Integer [] hotelId);
+    int addHotel(Hotel hotel);
+    int updateByHotelId(Hotel hotel);
 }
